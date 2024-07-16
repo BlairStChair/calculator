ee9ac89700f52fd369dce171af6745c296874796
BlairStChair <julia.z15@interia.pl>
2024-07-16 14:14:37 +0200

    Created button's structure

--- script.js
+++ script.js
@@ -33,3 +33,20 @@ function operate(){
     }
 
 }
+
+const zero = document.querySelector("zero");
+const one = document.querySelector("one");
+const two = document.querySelector("two");
+const three = document.querySelector("three");
+const four = document.querySelector("four");
+const five = document.querySelector("five");
+const six = document.querySelector("six");
+const seven = document.querySelector("seven");
+const eight = document.querySelector("eight");
+const nine = document.querySelector("nine");
+const addition = document.querySelector("addition");
+const substraction = document.querySelector("substraction");
+const multiplying = document.querySelector("multiplying");
+const division = document.querySelector("division");
+const output = document.querySelector("output");
+const clear = document.querySelector("clear");

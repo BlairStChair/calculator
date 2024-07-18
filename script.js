f96232f7031bfd49174ccd723962ecf850377da3
BlairStChair <julia.z15@interia.pl>
2024-07-18 18:16:55 +0200

    Added functionality to be able to add more than two numbers

--- script.js
+++ script.js
@@ -24,26 +24,140 @@ let firstNumber = 0;
 let firstNumberLength = -1;
 let operator = '';
 let secondNumber = 0;
+let secondNumberLength = 0;
+let result = 0;
+let equationArray = [];
+
+let operatorClickCounter = 0;
 
 function operate(){
-    if(operator == '+'){
-        return add(firstNumber, secondNumber);
+    const num1 = parseInt(firstNumber, 10);
+    const num2 = parseInt(secondNumber, 10);
+    
+    if(operator === '+'){
+        return add(num1, num2);
+    } else if(operator === '-'){
+        return subtract(num1, num2);
+    } else if(operator === '*'){
+        return multiply(num1, num2);
+    } else if(operator === '/'){
+        return divide(num1, num2);
     }
-    else if(operator == '-'){
-        return subtract(firstNumber, secondNumber);
-    }
-    else if(operator == '*'){
-        return multiply(firstNumber, secondNumber);
-    }
-    else if(operator == '/'){
-        return divide(firstNumber, secondNumber);
-    }
-
 }
 
-const equationArray = [];
-
 const display = document.querySelector("#display");
+
+const output = document.querySelector("#output");
+output.addEventListener("click", () =>{
+    firstNumberLength = equationArray.reduce((acc, curr) => {
+        if (acc.stop || typeof curr !== 'number') {
+            acc.stop = true;
+            return acc;
+        }
+        acc.length++;
+        return acc;
+    }, { length: 0, stop: false }).length;
+
+    secondNumberLength = equationArray.length - (firstNumberLength + 1);
+    
+    firstNumber = parseInt(equationArray.slice(0, firstNumberLength).join(''));
+    secondNumber = parseInt(equationArray.slice(firstNumberLength + 1, equationArray.length).join(''));
+
+    if(equationArray[0] === "+" || equationArray[0] === "-" || equationArray[0] === "*" || equationArray[0] === "/"){
+        display.style.color = "red";
+        display.textContent = "You can't start with an operator!";
+    }else{
+        display.textContent = operate();
+        result = operate();
+        equationArray = [result];
+        firstNumber = result;
+        secondNumber = 0;
+        operator = '';
+        operatorClickCounter = 0;
+    }
+});
+
+const addition = document.querySelector("#a");
+addition.addEventListener("click", () =>{
+    if(operatorClickCounter > 0){
+        firstNumberLength = equationArray.reduce((acc, curr) => {
+            if (acc.stop || typeof curr !== 'number') {
+                acc.stop = true;
+                return acc;
+            }
+            acc.length++;
+            return acc;
+        }, { length: 0, stop: false }).length;
+    
+        secondNumberLength = equationArray.length - (firstNumberLength + 1);
+        
+        firstNumber = parseInt(equationArray.slice(0, firstNumberLength).join(''));
+        secondNumber = parseInt(equationArray.slice(firstNumberLength + 1, equationArray.length).join(''));
+    
+        
+            display.textContent = firstNumber + secondNumber;
+            result = firstNumber + secondNumber;
+
+            console.log(display.textContent);
+            console.log(firstNumber);
+            console.log(secondNumber);
+            
+            const additionD = document.createElement("p");
+            additionD.classList.add("numbersClass"); 
+             
+             additionD.textContent = "+";
+             display.appendChild(additionD);
+             equationArray = [];
+             equationArray[0] = result;
+             equationArray.push("+");
+
+
+             console.log(equationArray.toString());
+             operatorClickCounter++;
+    }
+    else{
+        const additionD = document.createElement("p");
+        additionD.classList.add("numbersClass"); 
+        additionD.textContent = "+";
+        display.appendChild(additionD);
+        equationArray.push("+");
+        operator = "+";
+        operatorClickCounter++;
+    }
+});
+
+const subtraction = document.querySelector("#s");
+subtraction.addEventListener("click", () =>{
+    const subtractionD = document.createElement("p");
+    subtractionD.classList.add("numbersClass"); 
+    subtractionD.textContent = "-";
+    display.appendChild(subtractionD);
+    equationArray.push("-");
+    operator = "-";
+    operatorClickCounter++;
+});
+
+const multiplying = document.querySelector("#m");
+multiplying.addEventListener("click", () =>{
+    const multiplyingD = document.createElement("p");
+    multiplyingD.classList.add("numbersClass"); 
+    multiplyingD.textContent = "*";
+    display.appendChild(multiplyingD);
+    equationArray.push("*");
+    operator = "*";
+    operatorClickCounter++;
+});
+
+const division = document.querySelector("#d");
+division.addEventListener("click", () =>{
+    const divisionD = document.createElement("p");
+    divisionD.classList.add("numbersClass"); 
+    divisionD.textContent = "/";
+    display.appendChild(divisionD);
+    equationArray.push("/");
+    operator = "/";
+    operatorClickCounter++;
+});
 
 const zero = document.querySelector("#zero");
 zero.addEventListener("click", () =>{
@@ -145,74 +259,15 @@ nine.addEventListener("click", () =>{
     firstNumberLength++;
 });
 
-const addition = document.querySelector("#a");
-addition.addEventListener("click", () =>{
-    const additionD = document.createElement("p");
-    additionD.classList.add("numbersClass"); 
-    additionD.textContent = "+";
-    display.appendChild(additionD);
-    equationArray.push("+");
-    operator = "+";
-});
-
-const subtraction = document.querySelector("#s");
-subtraction.addEventListener("click", () =>{
-    const subtractionD = document.createElement("p");
-    subtractionD.classList.add("numbersClass"); 
-    subtractionD.textContent = "-";
-    display.appendChild(subtractionD);
-    equationArray.push("-");
-    operator = "-";
-});
-
-const multiplying = document.querySelector("#m");
-multiplying.addEventListener("click", () =>{
-    const multiplyingD = document.createElement("p");
-    multiplyingD.classList.add("numbersClass"); 
-    multiplyingD.textContent = "*";
-    display.appendChild(multiplyingD);
-    equationArray.push("*");
-    operator = "*";
-});
-
-const division = document.querySelector("#d");
-division.addEventListener("click", () =>{
-    const divisionD = document.createElement("p");
-    divisionD.classList.add("numbersClass"); 
-    divisionD.textContent = "/";
-    display.appendChild(divisionD);
-    equationArray.push("/");
-    operator = "/";
-});
-
-
-
-const output = document.querySelector("#output");
-output.addEventListener("click", () =>{
-    firstNumberLength = equationArray.reduce((acc, curr) => {
-        if (acc.stop || typeof curr !== 'number') {
-            acc.stop = true;
-            return acc;
-        }
-        acc.length++;
-        return acc;
-    }, { length: 0, stop: false }).length;
-    
-    firstNumber = equationArray.slice(0, firstNumberLength).join('');
-
-    secondNumber = equationArray.slice(firstNumberLength+1,equationArray.length).join('');
-
-    if(equationArray[0] === "+" || equationArray[0] === "-" || equationArray[0] === "*" || equationArray[0] === "/"){
-        display.style.color = "red";
-        display.textContent = "You can't start with an operator!";
-    }else{
-        display.textContent = operate();
-    }
-});
-
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", () =>{
     display.textContent = "";
+    firstNumber = 0;
+    firstNumberLength = 0;
+    secondNumber = 0
+    secondNumberLength = 0;
+    operatorClickCounter = 0;
+    equationArray = [];
 });
 
 
